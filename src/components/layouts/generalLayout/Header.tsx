0d83354ac6451594dd55e1ee6a7b2367598e3617
raulherreraa19 <raul_herrera@empress.com.mx>
2026-01-13 16:17:48 -0600

    changes

--- src/components/layouts/generalLayout/Header.tsx
+++ src/components/layouts/generalLayout/Header.tsx
@@ -1,26 +1,19 @@
 
 const Header = () => {
     return (
-        <header className="container mx-auto p-4">
-            <nav className="mt-4 mb-4 flex justify-end">
-                <ul className="flex space-x-4" >
-                    <li className=""><a href="/">Inicio</a></li>
-                    <li><a href="/features">Características</a></li>
-                    <li><a href="/pricing">Precios</a></li>
-                    <li><a href="/contact">Contacto</a></li>
-                </ul>
-                                
-            </nav>
-            <div className="flex-col w-full">
-                <div className="row-auto  bg-red-900">
-                    <h1 className="text-2xl font-bold">Bienvendio a AhorraYa</h1>  
-                    <p className="">Tu plataforma para gestionar y optimizar tus ahorros de manera eficiente.</p>
+        <header className=" w-full border-2 bg-grey-100 border-gray-300 text-white h-16">                    
+            <nav className="flex items-center justify-end">            
+                  <div className="grow">
+                    <img src="/logo.png" alt="AhorraYa Logo" className="h-15 w-20 ml-10"/>
                 </div>
-                <div className="row">
-                    <h1 className="text-2xl font-bold">Bienvendio a AhorraYa</h1>  
-                    <p className="">Tu plataforma para gestionar y optimizar tus ahorros de manera eficiente.</p>
-                </div>
-            </div>
+                <ul className="flex space-x-2 mr-10" >
+                    <li className="text-black rounded-lg px-3 py-2 transition-transform duration-200 hover:scale-105 hover:bg-gray-300"><a href="/#">Inicio</a></li>
+                    <li className="text-black rounded-lg px-3 py-2 transition-transform duration-200 hover:scale-105 hover:bg-gray-300"><a href="/#">Características</a></li>
+                    <li className="text-black rounded-lg px-3 py-2 transition-transform duration-200 hover:scale-105 hover:bg-gray-300"><a href="/#">Precios</a></li>
+                    <li className="text-black rounded-lg px-3 py-2 transition-transform duration-200 hover:scale-105 hover:bg-gray-300"><a href="/#">Contacto</a></li>
+                    <li className="text-black rounded-lg px-3 py-2 transition-transform duration-200 hover:scale-105 hover:bg-gray-300"><a href="/login">Iniciar Sesión</a></li>
+                </ul>                                
+            </nav>          
         </header>
     );
 }
